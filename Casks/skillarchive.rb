b2ee2dc7ac8c83378e7f5f799c5a65db8279cf76
mrKangHo <coke8707@gmail.com>
2026-09-03 10:13:03 +0900

    Bump Cask to v1.0.1

--- Casks/skillarchive.rb
+++ Casks/skillarchive.rb
@@ -1,6 +1,6 @@
 cask "skillarchive" do
-  version "1.0.0"
-  sha256 "017d2454637629da38542d670b01131ede648d223734328dc79202a3599dcdf2"
+  version "1.0.1"
+  sha256 "67ff265918090fecbfaad575f2aaf729a5787f1248cac51cf7107c392bc5c510"
 
   url "https://github.com/mrKangHo/SkillArchive/releases/download/v#{version}/SkillArchive.app.zip"
   name "SkillArchive"
